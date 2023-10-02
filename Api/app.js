40bc00645250f7726cddb570a6bde82570b36d7e
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-02 09:19:14 -0500

    feat 🏗️ Change file location
    - Cahnege location.
    - Add mongodb connetion.
    - add data insert
    - add enviroment variables.

---
+++ Api/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { config } = require('./config/config');
+const { URI } = require('./libs/mongoDB/data/conexion');
+const { getCourseData } = require('./libs/mongoDB/data/import/processApi');
+
+dotenv.config();
+const app = express();
+
+const PORT = config.port;
+const hostname = config.host;
+
+app.use(express.json());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server on: http://${hostname}:5161`);
+  console.log(`Mongo link: ${URI}`);
+});
+
+// getCourseData()
