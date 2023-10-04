3b7d95ce3e4b8f6efde6a751a193606c7d7d028c
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-04 16:17:18 -0500

    feat 🚧 Create footer component
    - Has app logo
    - Has a short message to inspire
    - Has developer reference

---
+++ src/components/Principal/footer/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './style.css'
+import { AboutMe } from './AboutMe/index.jsx'
+import InteretelarLogoGif from '../../../public/logo.svg';
+import { AboutProject } from "./AboutProject";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="column">
+        <div className="logo-container">
+      <img  src={InteretelarLogoGif} alt="Interetelar Logo" className="logo-image" />
+    </div>
+      </div>
+      <div className="column project">
+        <AboutProject/>
+      </div>
+      <div className="column">
+        <AboutMe/>
+      </div>
+    </footer>
+  );
+}
+
+export { Footer };
