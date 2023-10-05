db2660e8c8af700f29b4c54f2bb3a3adbfb7b89b
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-05 01:13:55 -0500

    feat 🚧 Crea login view.

--- src/components/app/Views/loginUI.jsx
+++ src/components/app/Views/loginUI.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Context } from "../../contex/index.jsx";
+import { Login } from "../../login/index.jsx";
 import { Footer } from "../../footer/index.jsx";
 
 function LoginUi(){
 React.useContext(Context)
 return(
     <>
-        
+        <Login/>
         <Footer></Footer>
     </>
 );
