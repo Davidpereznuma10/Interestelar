47e95f816a32bbb7339ade9c42cc37fb7142ad86
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-04 20:55:44 -0500

    feat 🚧 Create sidebar
    - Create the buttons to open a close the sidebar
    - Create a list to render the discord documentation.
    - Create a button to logout.

---
+++ src/components/Principal/sidebar/button/icons/close.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
+
+function Close(){
+    return(
+        <RiCloseCircleLine className="icons"/>
+    )
+}
+
+export { Close }
