50ec50a752b6c52baca5fbf05b3f041541fa95db
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-04 22:38:56 -0500

    feat 🚧 Create views
    - Create Home view.
    - Create login view.
    - Create video view.

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,20 @@
 import React from "react"
-import { AppUi } from "./AppUi"
+import { LoginUi } from "./Views/loginUI"
+import { HomeUi } from "./Views/HomeUi"
+import { VideoUi } from "./Views/videosUI"
 import { Provider } from "../contex"
+import { Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <Provider>
-      <AppUi/>
-    </Provider>
+    <>
+      <Provider>  
+        <Routes>
+          <Route path="/Login" element={<LoginUi />} />
+          <Route path="/Home" element={<HomeUi />}/>
+          <Route path="/Video" element={<VideoUi />} />
+        </Routes>
+      </Provider>
+    </>
   )
 }
 export { App }
