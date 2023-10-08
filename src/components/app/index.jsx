04b4d43f53d189682895f0969313eb86586bef0e
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-08 16:04:08 -0500

    feat 🚚 Update principal router
    - This is because I want that when someone goes to the main path, it redirects them to the login page.

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,18 +3,21 @@ import { LoginUi } from "./Views/loginUI"
 import { HomeUi } from "./Views/HomeUi"
 import { VideoUi } from "./Views/videosUI"
 import { Provider } from "../contex"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
+
 function App() {
   return (
     <>
-      <Provider>  
+      <Provider>
         <Routes>
+          <Route path="/" element={<Navigate to="/Login" />} />
           <Route path="/Login" element={<LoginUi />} />
-          <Route path="/Home" element={<HomeUi />}/>
+          <Route path="/Home" element={<HomeUi />} />
           <Route path="/Video" element={<VideoUi />} />
         </Routes>
       </Provider>
     </>
   )
 }
+
 export { App }
