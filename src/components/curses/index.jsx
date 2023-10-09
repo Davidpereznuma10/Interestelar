c44f28792b1dfc2443bf1cf2b811dd0c2c6dc467
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-09 12:22:53 -0500

    feat 🚧 Contex courses
    - Pass all the data into the contex and call it through the HomeUI.

--- src/components/curses/index.jsx
+++ src/components/curses/index.jsx
@@ -1,50 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; // Importa el componente Link
+import React from "react";
+import { Link } from "react-router-dom"; 
 import { Image } from "./images/index.jsx";
 import { Content } from "./content/index.jsx";
-import axios from 'axios';
 import './style.css';
 
-function Curses() {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    try {
-      const response = await axios.get('http://127.9.63.7:5161/api/v1/courses');
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      return [];
-    }
-  };
-
-  useEffect(() => {
-    async function fetchData() {
-      const coursesData = await getData();
-      setData(coursesData);
-    }
-    fetchData();
-  }, []);
-
-  const [isHovered, setIsHovered] = useState(new Array(6).fill(false));
-
-  const handleMouseEnter = (index) => {
-    const updatedIsHovered = [...isHovered];
-    updatedIsHovered[index] = true;
-    setIsHovered(updatedIsHovered);
-  };
-
-  const handleMouseLeave = (index) => {
-    const updatedIsHovered = [...isHovered];
-    updatedIsHovered[index] = false;
-    setIsHovered(updatedIsHovered);
-  };
+function Curses({data, handleMouseEnter, handleMouseLeave, isHovered }) {
 
   return (
     <div className="container-wrapper">
         {data.map((course, index) => (
           <Link
             key={index} 
-            to={`/Video?course=${course.apiCourseName}&section=${course.sections[0]?.sectionName || ''}&video=${course.sections[0]?.videos[0]?.nombreDelVideo || ''}`}
+            to={`/Multimedia/${course.courseName}`}
             className={`container-curses ${isHovered[course.id] ? 'hovered' : ''}`}
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave(index)}
