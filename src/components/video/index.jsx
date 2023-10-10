1bfb4917ef485df35f6decb19740c8321806ad8f
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-10-10 07:07:18 -0500

    feat 🚧 Call resources components
    - Using prop call all the module components and rederized them into the principal component.

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,23 +1,30 @@
-import React, { useRef } from "react";
+import React, { useRef} from "react";
 import "./style.css";
 import { CustomerRows } from "./constumer/index.jsx";
 
-function Video() {
+function Video({ activeTab, links, comments, videos, handleTabClick, altValue, handleLinkClick }) {
   const videoRef = useRef(null);
-
   return (
     <>
         <div className="video-container">
             <video ref={videoRef} controls>
-                <source src="#" type="video/mp4" />
+                <source src={altValue} type="video/mp4" />
                 Tu navegador no soporta la reproducción de video.
             </video>
         </div>
         <div className="video-costumer">
-            <CustomerRows/>
+            <CustomerRows
+            activeTab={activeTab}
+            links={links}
+            comments={comments}
+            videos={videos}
+            handleTabClick={handleTabClick}
+            handleLinkClick={handleLinkClick} // Asegúrate de pasar handleLinkClick aquí
+            />
         </div>
     </>
   );
 }
+
 
 export { Video };
